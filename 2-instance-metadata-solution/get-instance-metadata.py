25a47b87f27fe1230dec1817c466dd3ee7214f43
Yuvraj A <Yuvraj A>
2023-01-22 20:28:00 +0530

    added solution 2

--- 2-instance-metadata-solution/get-instance-metadata.py
+++ 2-instance-metadata-solution/get-instance-metadata.py
@@ -1,27 +1,45 @@
-# def flat_dict(sample_dict):
-#   out_dict = {}
- 
-#   for key, val in sample_dict.items():
-#     if type(val) == dict:
-#       out_dict[key] = len(sample_dict.keys())
-#       out_dict.update(val)
-#     else:
-#       out_dict[key] = val
- 
-#   return out_dict
+import requests
+import json
 
+# metadata url
+metadata_url = "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
 
-# if __name__ == "__main__":
-#     sample_dict = {"a":{"b":{"c":"d"}}}
-#     out_dict = flat_dict(sample_dict)
-#     print(out_dict)
+def get_key_data(inputObject, key):
+    compute_json = inputObject['compute']
+    network_json = inputObject['network']
 
-def recursive_lookup(k, d):
-    if k in d:
-        return d[k]
-    for v in d.values():
-        if isinstance(v, dict):
-            return recursive_lookup(k, v)
-    return None
+    if key in compute_json.keys():
+        return compute_json.get(key)
+    elif key in network_json.get(key):
+        return network_json.get(key)
+    else:
+        return "key not found"
 
-print(recursive_lookup('b', {"a":{"b":{"c":"d"}}}))
+def get_metadata():
+    # metadeta url only works inside a VM, for testig i have below script.
+
+    # with open('meta-data.json', 'r') as f:
+    #     metadata_to_dict = json.load(f)
+    # return metadata_to_dict
+
+    # making request to get the Azure VM metadata
+    # returning the metadata to dictionay.
+
+    response = requests.get(metadata_url, headers={"Metadata":"true"})
+    metadata_to_dict = response.json()  
+    return metadata_to_dict
+
+def get_metadata_json():
+    # converting the meta data from dictionary to json
+    metadata_to_json = json.dumps(get_metadata(), indent=4)
+    return metadata_to_json  
+
+if __name__ == '__main__':
+    # getting reading and getting data:
+    print(get_metadata_json())
+
+    # getting the data from the json based on key
+    # reading the key as input
+    input_key = input("provide the key to get the meta data : ")
+    key_data = get_key_data(get_metadata(),input_key)
+    print(key_data)
